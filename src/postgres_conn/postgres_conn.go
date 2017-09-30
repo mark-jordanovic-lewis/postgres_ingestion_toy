eef26a2b665ee666b3f91dbd7e11eba6c3e9f8cd
morbidslug <mark.4ndrew.lewis@gmail.com>
2017-09-30 21:10:04 +0200

    finished rng of database fields, started connections to postgres

---
+++ src/postgres_conn/postgres_conn.go
@@ -0,0 +1,35 @@
+package postgres_conn
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	pgx "github.com/jackc/pgx"
+	pq "github.com/lib/pq"
+)
+
+func MakeConnection() *sql.DB {
+	conn, err := sql.Open("postgres", "user=maruko dbname=swarmtest")
+	if err != nil {
+		fmt.Println("Could not connect to database")
+		os.Exit(1)
+	}
+	return conn
+}
+
+func tmp_pq(db *sql.DB) {
+	txn, err := db.Begin()
+	txn.Prepare(pq.CopyIn("ingestion_test", "src", "dst", "flags"))
+}
+
+func tmp_pgx(db *sql.DB) {
+	txn, err := db.Begin()
+	rows := []DataFields{}
+
+	copyCount, err := conn.CopyFrom(
+		pgx.Identifier{"ingestion_test"},
+		[]string{"src", "dst", "flags"},
+		pgx.CopyFromRows(rows),
+	)
+}
---
+++ src/postgres_conn/postgres_conn_test.go
@@ -0,0 +1,11 @@
+package postgres_conn
+
+import "testing"
+
+func TestMakeConnection(t *testing.T) {
+	t.Log("Checking connection to postgres database")
+	conn := MakeConnection()
+	if conn == nil {
+		t.Errorf("Expected connection to postgres but got %v", conn)
+	}
+}
