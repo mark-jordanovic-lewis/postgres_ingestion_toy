48749d1748769f3dca1ffc5ade3bdbd9586734f1
morbidslug <mark.4ndrew.lewis@gmail.com>
2017-10-04 18:06:18 +0200

    made first attempt at benchmarking, did not realise timestamp would be the same for all data, thought row write time would be stamped

--- src/main/main.go
+++ src/main/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"calculator"
+	"fmt"
 	gen "generator"
 	alpha "pq_conn"
 )
 
+// TODO: add interface so can pass all XXXConnection types to copy rate calc
+
 func main() {
+	var row_rate float64
 	data_set := gen.NewDataSet(100000)
-	pq_conn := alpha.MakeConnection("swarm_benchmark")
+	pq_conn := alpha.MakeConnection("swarmtest", "ingestion_test")
 	pq_conn.OpenTransaction()
+	// change to for, concurrentify
 	if pq_conn.ConnectionOpen {
+		fmt.Println("Connection Opened")
 		pq_conn.IngestData(data_set)
+		if pq_conn.BatchIngested {
+			fmt.Println("Batch Ingested")
+			timestamps := pq_conn.SelectTimeStamps()
+			fmt.Println(timestamps)
+			row_rate = calculator.CalculatePqBatchCopyRate(timestamps)
+		}
+		pq_conn.DropAllRows()
 	}
+
+	fmt.Printf("%v rows/s\n", row_rate)
+
 }
