ba28402d21ba3f1ed6ad86b453902cdba0a759bc
morbidslug <mark.4ndrew.lewis@gmail.com>
2017-10-06 00:46:00 +0200

    made general calculator and histogram drawer

--- src/main/main.go
+++ src/main/main.go
@@ -1,35 +1,51 @@
 package main
 
 import (
+	dt "connections/connectionTimer"
 	alpha "connections/pq_conn"
 	"fmt"
 	gen "generator"
 	hist "histogram"
+	"math"
 	"math/rand"
 	"time"
 )
 
 // TODO: add interface so can pass all XXXConnection types to copy rate calc
 
-var source = rand.NewSource(time.Now().UnixNano())
-var rng = rand.New(source)
-
 func main() {
-	var row_per_s, mu_rps float64
-	m_batches := rand.Intn(10) // 10000
+	var mu_rpns float64
+	var m_times []float64
+	rand.Seed(time.Now().UnixNano())
+
+	m_batches := rand.Intn(9999) + 1 // 10000
 
 	pq_conn := alpha.MakeConnection("swarmtest", "ingestion_test")
 
 	for m := 0; m < m_batches; m++ {
-		n_rows := rand.Intn(100) // 100000
+		if m == 0 {
+			m_times = make([]float64, m_batches)
+		}
+		n_rows := rand.Intn(999) + 1 // 100000
 		data_set := gen.NewDataSet(n_rows)
-		dt := float64(
-			hist.generateHistogramIO(data_set, pq_conn))
-		mu_rps += float64(
-			n_rows) / dt_ns
-		pq_conn.DropAllRows()
-	}
-	mu_rps /= float64(m_batches)
+		rpns := float64(n_rows) / dt.TimeIngestion(data_set, pq_conn)
+		m_times[m] = rpns * math.Pow10(9)
+		mu_rpns += rpns
 
-	fmt.Printf("mean rows/s: %v\n", mu_rps)
+		if pq_conn.BatchIngested() {
+			fmt.Printf("Batch Ingested: %v of %v\n", m, m_batches)
+			fmt.Printf("rows: %v, rows/s: %v\n", n_rows, rpns*math.Pow10(9))
+		} else {
+			fmt.Printf("Batch Rejected: %v of %v\n    !!!", m, m_batches)
+		}
+		pq_conn.DropAllRows()
+		fmt.Println()
+	}
+	hist.BuildHistogram("first.png", m_times)
+	fmt.Println("Drawing histogram")
+
+	mu_rpns /= float64(m_batches)
+	mu_rps := mu_rpns * math.Pow10(9)
+	fmt.Println()
+	fmt.Printf("Mean rows/s: %v\n", mu_rps)
 }
