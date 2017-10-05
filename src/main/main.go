582ac6cf8b1952427c700872fe8d93c4401e2290
morbidslug <mark.4ndrew.lewis@gmail.com>
2017-10-05 02:17:02 +0200

    refactor for histogram

--- src/main/main.go
+++ src/main/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	gen "generator"
-	"math"
 	"math/rand"
 	alpha "pq_conn"
 	"time"
@@ -23,25 +22,13 @@ func main() {
 	for m := 0; m < m_batches; m++ {
 		n_rows := rand.Intn(100000)
 		data_set := gen.NewDataSet(n_rows)
-		pq_conn.OpenTransaction()
-		// change to for, concurrentify
-		if pq_conn.ConnectionOpen {
-			fmt.Println("Connection Opened")
-			start := time.Now()
-			pq_conn.IngestData(data_set)
-			end := time.Now()
-
-			if pq_conn.BatchIngested {
-				fmt.Println("Batch Ingested")
-				dt_ns := (end.UnixNano() - start.UnixNano())
-				dt_s := float64(dt_ns) / math.Pow10(9)
-				row_per_s = float64(n_rows) / dt_s
-				fmt.Printf("rows/s: %v\n", row_per_s)
-				mu_rps += row_per_s
-			}
-		}
-		// pq_conn.DropAllRows()
+		dt := float64(
+			generateHistogramIO(data_set, pq_conn))
+		mu_rps += float64(
+			n_rows) / dt_ns
+		pq_conn.DropAllRows()
 	}
 	mu_rps /= float64(m_batches)
+
 	fmt.Printf("mean rows/s: %v\n", mu_rps)
 }
